234ae4605b6c6b8a9e65d0cb549e88deec80f6a0
Israel <israelpina047@gmail.com>
2024-09-11 18:44:08 -0300

    feat: add new season contact of project.

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
       </nav>
     </div>
   </header>
