e3ff0f3d1e8f137fd2fcfc31a5535dd1a33d6f34
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-11 10:05:08 -0300

    update: Create initial Home

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {RouterLink} from "vue-router";
+import {Button} from "../components/ui/button";
+</script>
+
+<template>
+    <main class="container">
+        <RouterLink to="/about">
+            <Button>
+                About
+            </Button>
+        </RouterLink>
+    </main>
+</template>
