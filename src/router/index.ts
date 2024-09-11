0b354c5cb14bdd401edd856405a6fa3825231f03
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-11 10:11:07 -0300

    update: Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: Home,
-        },
-        {
-            path: "/about",
-            name: "about",
-            component: About,
-        },
-    ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: Home,
+		},
+		{
+			path: "/about",
+			name: "about",
+			component: About,
+		},
+	],
 });
 
 export default router;
