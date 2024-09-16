ed9ce7637f8d4330a5ace7774cda1b4e9a59e831
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-16 14:29:53 -0300

    bomb commit

--- src/components/Badges.vue
+++ src/components/Badges.vue
@@ -1,40 +1,40 @@
 <script lang="ts">
 import Image from "@/tags/Image.vue";
-import {BadgesDto} from "./Dto/BadgesDto";
+import { BadgesDto } from "./Dto/BadgesDto";
 import Skeleton from "@/components/ui/skeleton/Skeleton.vue";
 import Link from "@/tags/Link.vue";
 import HoverCard from "@/components/HoverCard.vue";
 import ImageSkeletonSvg from "@/components/Svg/ImageSkeletonSvg.vue";
-import type {PropType} from "vue";
+import type { PropType } from "vue";
 
 export interface BadgeProps {
-	name: string;
-	occupation: string;
-	github: string;
-	nickname: string;
-	description: string;
-	startDate: string;
-	initialLetters: string;
+    name: string;
+    occupation: string;
+    github: string;
+    nickname: string;
+    description: string;
+    startDate: string;
+    initialLetters: string;
 }
 
 export default {
-	props: {
-		badges: {
-			type: Array as PropType<BadgeProps[]>,
-			default: () => BadgesDto,
-		},
-	},
-	data() {
-		return {
-			loading: true,
-		};
-	},
-	components: {Skeleton, Image, Link, HoverCard, ImageSkeletonSvg},
-	methods: {
-		onImageLoad() {
-			this.loading = false;
-		},
-	},
+    props: {
+        badges: {
+            type: Array as PropType<BadgeProps[]>,
+            default: () => BadgesDto,
+        },
+    },
+    data() {
+        return {
+            loading: true,
+        };
+    },
+    components: { Skeleton, Image, Link, HoverCard, ImageSkeletonSvg },
+    methods: {
+        onImageLoad() {
+            this.loading = false;
+        },
+    },
 };
 </script>
 
@@ -45,7 +45,7 @@ export default {
                 <Skeleton v-if="loading" class="w-20 h-20 rounded-full">
                     <ImageSkeletonSvg />
                 </Skeleton>
-                <Link :href="badge.github" target="_blank" class="hover:opacity-50 duration-150 ease-in-out">
+                <Link :href="badge.github" target="_blank">
                 <Image :src="badge.github + '.png'" :alt="badge.name + ' photo'" width="80" height="80"
                     class="rounded-full" @load="onImageLoad()" />
                 </Link>
