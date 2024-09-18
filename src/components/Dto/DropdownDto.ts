057890548185ef3312d8c4776061105a268937ec
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-18 20:13:39 -0300

    feat: add new elements coins to file DropdonwDto.ts

--- src/components/Dto/DropdownDto.ts
+++ src/components/Dto/DropdownDto.ts
@@ -1,35 +1,46 @@
-import type {Menu} from "../Dropdown.vue";
+import type { Menu } from "../Dropdown.vue";
 
 export const DropdownDto: Menu[] = [
-	{
-		title: "Cripto moedas",
-		link: "/criptos",
-		subContent: [
-			{
-				title: "Bitcoin",
-				link: "/criptos?coin=bitcoin",
-				icon: "bitcoin",
-			},
-			{
-				title: "Ethereum",
-				link: "/criptos?coin=ethereum",
-				icon: "ethereum",
-			},
-			{
-				title: "Dogecoin",
-				link: "/criptos?coin=dogecoin",
-				icon: "dogecoin",
-			},
-		],
-	},
-	{
-		title: "Moedas globais",
-		link: "/moedas",
-		subContent: null,
-	},
-	{
-		title: "Skins CS:GO",
-		link: "/skins",
-		subContent: null,
-	},
+    {
+        title: "Cripto moedas",
+        link: "/criptos",
+        subContent: [
+            {
+                title: "Bitcoin",
+                link: "/criptos?coin=bitcoin",
+                icon: "bitcoin",
+            },
+            {
+                title: "Ethereum",
+                link: "/criptos?coin=ethereum",
+                icon: "ethereum",
+            },
+            {
+                title: "Dogecoin",
+                link: "/criptos?coin=dogecoin",
+                icon: "dogecoin",
+            },
+        ],
+    },
+    {
+        title: "Moedas globais",
+        link: "/coins",
+        subContent: [
+            {
+                title: "Dollar",
+                link: "/coins?coin=dollar",
+                icon: "dollar",
+            },
+            {
+                title: "Euro",
+                link: "/coins?coin=euro",
+                icon: "Euro",
+            },
+        ]
+    },
+    {
+        title: "Skins CS:GO",
+        link: "/skins",
+        subContent: null,
+    },
 ];
