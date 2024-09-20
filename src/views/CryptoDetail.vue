6e25c41a8d09a387ee0165f7c704a0243335294e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-19 21:10:50 -0300

    feat: Create initial crypto detail view

---
+++ src/views/CryptoDetail.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+const crypto = String(route.params.crypto);
+
+const capitalizeFirstLetter = (string: string) => {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+document.title = `${capitalizeFirstLetter(crypto)} | NEI Market Analytics`;
+</script>
+
+<template>
+    <main class="container justify-center">
+        <h1 class="hover:underline hover:opacity-50 ease-in-out duration-150">{{ crypto }}</h1>
+    </main>
+</template>
