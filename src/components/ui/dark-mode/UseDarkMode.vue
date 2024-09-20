7fad32ff43e3bfaba3c1c93355dd979ded1b21be
Erackson Souza <souza.erackson@gmail.com>
2024-09-20 15:56:40 -0300

    build: Created UseDarkMode that changes the theme color when clicking (dark and light)

---
+++ src/components/ui/dark-mode/UseDarkMode.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+import { watchEffect, ref } from "vue";
+
+import { Icon } from "@iconify/vue";
+
+const savedTheme = localStorage.getItem("theme");
+const isDark = ref(savedTheme === "dark" || savedTheme === null);
+
+// Função para alternar o tema
+const toggleDark = useToggle(isDark);
+
+watchEffect(() => {
+    if (isDark.value) {
+        document.body.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+    } else {
+        document.body.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+    }
+});
+</script>
+
+<template>
+    <button @click="toggleDark()" aria-label="Toggle dark mode">
+        <!-- Colocar isso em estado para mudar de icone, e consequentemente mudar o tema  -->
+        <a v-if="isDark">🌙</a>
+        <a v-else>🌞</a>
+    </button>
+</template>
+
+<style>
+body{
+    transition: background-color 0.3s ease-in-out, color 0.2s ease-in-out;
+}
+body.dark {
+    background: #f7fafd;
+    color: #1f2137;
+}
+
+button {
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+
+button:focus {
+    outline: none;
+}
+
+</style>
