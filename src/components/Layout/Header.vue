cd4a1127c1aa8cdcc181de380db9d0b8d1c8884e
Erackson Souza <souza.erackson@gmail.com>
2024-09-20 15:58:31 -0300

    update: Added UseDarkMode component to Header.

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -2,6 +2,7 @@
 import Dropdown from "@/components/Dropdown.vue";
 import SearchBar from "@/components/SearchBar.vue";
 import Image from "@/tags/Image.vue";
+import UseDarkMode from "../ui/dark-mode/UseDarkMode.vue";
 import RouterLink from "@/tags/RouterLink.vue";
 import {DropdownDto} from "@/components/Dto/DropdownDto";
 import {useTranslation} from "@/config/composable/translate";
@@ -10,13 +11,15 @@ useTranslation();
 </script>
 
 <template>
+
     <header class="layout flex justify-between px-8 items-center min-w-full border-b" data-testid="Header">
         <div class="flex gap-4 items-center">
             <RouterLink to="/" class="hover:opacity-50">
                 <Image class="w-[150px] h-[53.69px]" src="/logo.svg" alt="NEI Logo" width="150" height="53.69" />
             </RouterLink>
-            <Dropdown buttonTile="Mercados" label="Tópicos" :menu="DropdownDto" />
+            <Dropdown  buttonTile="Mercados" label="Tópicos" :menu="DropdownDto" />
         </div>
         <SearchBar />
+        <UseDarkMode />
     </header>
 </template>
