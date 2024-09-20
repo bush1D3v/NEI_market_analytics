84f2535318e57daa53d53e98e6fed62a35a0faec
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-19 21:10:57 -0300

    feat: Create initial coin detail view

---
+++ src/views/CoinDetail.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+const coin = String(route.params.coin);
+
+const capitalizeFirstLetter = (string: string) => {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+document.title = `${capitalizeFirstLetter(coin)} | NEI Market Analytics`;
+</script>
+
+<template>
+    <main class="container justify-center">
+        <h1 class="hover:underline hover:opacity-50 ease-in-out duration-150">{{ coin }}</h1>
+    </main>
+</template>
