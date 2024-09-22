7378a75ed7263345bb303429b8a2745064eafd00
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-21 21:28:23 -0300

    fix: Translator crashing the a tag

--- src/config/composable/translate.ts
+++ src/config/composable/translate.ts
@@ -1,27 +1,27 @@
-import {onMounted, onUpdated} from "vue";
+import { onMounted, onUpdated } from "vue";
 import vTranslate from "../directive/v-translate";
 
 export function useTranslation() {
-	const applyTranslation = () => {
-		const tagsToTranslate = ["span", "p", "h1", "h2", "h3", "h4", "a", "strong", "button"];
+    const applyTranslation = () => {
+        const tagsToTranslate = [ "span", "p", "h1", "h2", "h3", "h4", "strong", "button" ];
 
-		for (const tag of tagsToTranslate) {
-			const elements = document.querySelectorAll(tag);
-			for (const el of elements) {
-				if (el.closest(".accordion")) {
-					continue;
-				}
-				vTranslate.mounted(el as HTMLElement, {
-					value: (el as HTMLElement).innerText,
-					instance: null,
-					oldValue: undefined,
-					modifiers: {},
-					dir: vTranslate,
-				});
-			}
-		}
-	};
+        for (const tag of tagsToTranslate) {
+            const elements = document.querySelectorAll(tag);
+            for (const el of elements) {
+                if (el.closest(".accordion")) {
+                    continue;
+                }
+                vTranslate.mounted(el as HTMLElement, {
+                    value: (el as HTMLElement).innerText,
+                    instance: null,
+                    oldValue: undefined,
+                    modifiers: {},
+                    dir: vTranslate,
+                });
+            }
+        }
+    };
 
-	onMounted(applyTranslation);
-	onUpdated(applyTranslation);
+    onMounted(applyTranslation);
+    onUpdated(applyTranslation);
 }
