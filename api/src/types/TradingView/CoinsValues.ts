fb747117a289cf0056124a2c5bec845926c12c65
Israel Pina <israelpina047@gmail.com>
2024-10-03 23:13:55 -0300

    buid: add new functions, add new add api and new inplementation  the back end

---
+++ api/src/types/TradingView/CoinsValues.ts
@@ -0,0 +1,26 @@
+import type { Platform } from "../CoinMarketCap/Platform";
+import type { Quote } from "../CoinMarketCap/Quote";
+
+interface USDQuote {
+    USD: Quote;
+}
+
+export interface CoinsValues {
+    id: number;
+    name: string;
+    symbol: string;
+    slug: string;
+    num_market_pairs: number;
+    date_added: Date;
+    tags: string[];
+    max_supply: number;
+    circulating_supply: number;
+    total_supply: number;
+    is_active: number;
+    platform: Platform | null;
+    cmc_rank: number;
+    self_reported_circulating_supply: number | null;
+    self_reported_market_cap: number | null;
+    last_updated: Date;
+    quote: USDQuote;
+}
---
+++ app/stores/useCoinsWorldStore.ts
@@ -0,0 +1,35 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+import type { CurrencyValue } from "@/types/TradingView/CurrencyValue";
+
+export const useCryptoCurrencyStore = defineStore("coinsWorld", () => {
+    const coinsCurrencies = ref<CurrencyValue[]>([]);
+
+    function addCoinsCurrency(coinsCurrencyArray: CurrencyValue[]){
+         coinsCurrencies.value.push(...coinsCurrencyArray)
+    }
+    function removeCoinsCurrency(id: string): void {
+            coinsCurrencies.value = coinsCurrencies.value.filter(
+              (coinsCurrencies) => coinsCurrencies.id.toString() !== id,
+            )
+    }
+
+    function detailCoinsCurrencyByName(name: string): CurrencyValue | null {
+        const coinsCurrency = coinsCurrencies.value.find(
+            (coinsCurrency) => coinsCurrency.name === name,
+        );
+        if(!coinsCurrency) {
+            return null
+        }
+        return coinsCurrency
+    }
+
+    return {
+        coinsCurrencies,
+        addCoinsCurrency,
+        removeCoinsCurrency,
+        detailCoinsCurrencyByName
+    }
+
+})
