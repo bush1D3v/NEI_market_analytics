3fb667e19a521493141d8bc88fd41dc53e00370d
Erackson Souza <souza.erackson@gmail.com>
2024-10-10 21:45:39 -0300

    update : Added dto api stock

--- app/components/Dto/DropdownDto.ts
+++ app/components/Dto/DropdownDto.ts
@@ -1,76 +1,79 @@
-import type {Menu} from "../Dropdown.vue";
+import type { Menu } from "../Dropdown.vue";
 import Ethereum from "@/assets/images/ethereum.png";
 import Bitcoin from "@/assets/images/bitcoin.png";
 import Dogecoin from "@/assets/images/dogecoin.png";
 import Dollar from "@/assets/images/dollar.png";
 import Euro from "@/assets/images/euro.png";
 import Real from "@/assets/images/real.png";
-
+import B3 from "@/assets/images/logo-b3.png";
+import Vale from "@/assets/images/logo-vale.jpg";
+import Wege from "@/assets/images/logo-wege.png";
+import Petrobras from "@/assets/images/logo-petra.jpg";
 export const DropdownDto: Menu[] = [
-	{
-		emoji: "&#129689;",
-		title: "Cripto moedas",
-		link: "/cryptos",
-		subContent: [
-			{
-				title: "Bitcoin",
-				link: "/cryptos/bitcoin",
-				logo: Bitcoin,
-			},
-			{
-				title: "Ethereum",
-				link: "/cryptos/ethereum",
-				logo: Ethereum,
-			},
-			{
-				title: "Dogecoin",
-				link: "/cryptos/dogecoin",
-				logo: Dogecoin,
-			},
-		],
-	},
-	{
-		emoji: "&#128181;",
-		title: "Moedas globais",
-		link: "/coins",
-		subContent: [
-			{
-				title: "Dollar",
-				link: "/coins/dollar",
-				logo: Dollar,
-			},
-			{
-				title: "Real",
-				link: "/coins/real",
-				logo: Real,
-			},
-			{
-				title: "Euro",
-				link: "/coins/euro",
-				logo: Euro,
-			},
-		],
-	},
-	// {
-	//     emoji: "&#128299;",
-	//     title: "Skins CS2",
-	//     link: "/skins",
-	//     subContent: [
-	//         {
-	//             title: "Dragon Lore",
-	//             link: "/skins?skin=dragon-lore",
-	//             logo: "csgo",
-	//         },
-	//         {
-	//             title: "Butterfly",
-	//             link: "/skins?skin=butterfly",
-	//             logo: "csgo",
-	//         },
-	//         {
-	//             title: "Ak blue-gem",
-	//             link: "/skins?skin=ak-blue-gem",
-	//             logo: "csgo",
-	//         },
-	//     ],
-	// },
+    {
+        emoji: "&#129689;",
+        title: "Cripto moedas",
+        link: "/cryptos",
+        subContent: [
+            {
+                title: "Bitcoin",
+                link: "/cryptos/bitcoin",
+                logo: Bitcoin,
+            },
+            {
+                title: "Ethereum",
+                link: "/cryptos/ethereum",
+                logo: Ethereum,
+            },
+            {
+                title: "Dogecoin",
+                link: "/cryptos/dogecoin",
+                logo: Dogecoin,
+            },
+        ],
+    },
+    {
+        emoji: "&#128181;",
+        title: "Moedas globais",
+        link: "/coins",
+        subContent: [
+            {
+                title: "Dollar",
+                link: "/coins/dollar",
+                logo: Dollar,
+            },
+            {
+                title: "Real",
+                link: "/coins/real",
+                logo: Real,
+            },
+            {
+                title: "Euro",
+                link: "/coins/euro",
+                logo: Euro,
+            },
+        ],
+    },
+    {
+        emoji: "&#128203;",
+        title: "Bolsa de Valores",
+        link: "/stocks",
+        subContent: [
+            {
+                title: "Petrobras",
+                link: "/stocks/petrobras",
+                logo: Petrobras,
+            },
+            {
+                title: "Vale",
+                link: "/stocks/vale",
+                logo: Vale,
+            },
+            {
+                title: "Weg",
+                link: "/stocks/weg",
+                logo: Wege,
+            },
+        ],
+    },
 ];
