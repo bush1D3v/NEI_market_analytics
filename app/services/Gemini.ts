13640ea06bcacea298b8800e6e00250d36a9b53b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-11-26 20:46:16 -0300

    update: Add params to gemini history

--- app/services/Gemini.ts
+++ app/services/Gemini.ts
@@ -4,13 +4,22 @@ import { post } from "@/server/HttpClient";
  * @description Handles the request to send a prompt to gemini via socket.io
  *
  * @param {string} prompt - The prompt to generate the content
+ * @param {string} sessionId - The session id of the user
+ * @param {string} lastUserMessage - The last user message
+ * @param {string} lastBotMessage - The last bot message
  * @returns Promise<void>
  * @throws {Error} If the request to the proxy fails
  */
-export default async function generateContent(prompt: string, sessionId: string): Promise<void> {
+export default async function generateContent(prompt: string, sessionId: string, lastUserMessage?: string, lastBotMessage?: string): Promise<void> {
     const adaptedPrompt = `${prompt.trim()} /n Respond according to the user's language, directly and as briefly as possible. Text only.`;
     const body = {
         parts: [
+            {
+                text: lastUserMessage || "",
+            },
+            {
+                text: lastBotMessage || "",
+            },
             {
                 text: adaptedPrompt,
             },
